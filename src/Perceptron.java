e0f05ada8f541de5ed76808bd4e8dda51633dc15
u0173091 <u0173091@U0173091-TPD-A.ten.thomsonreuters.com>
2015-12-02 13:43:41 -0500

    s

---
+++ src/Perceptron.java
@@ -0,0 +1,33 @@
+
+public class Perceptron {
+	float[] weights;
+	float c = (float) 0.01;
+	  
+	Perceptron(int n) {
+		weights = new float[n];
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] = (float) Math.random();
+		}
+	}
+	
+	int feedforward(float[] inputs) {
+		float sum = 0;
+		for (int i = 0; i < weights.length; i++) {
+			sum += inputs[i]*weights[i];
+		}
+		return activate(sum);
+	}
+	
+	int activate(float sum) {
+		if (sum > 0) return 1;
+		else return -1;
+	}
+	
+	void train(float[] inputs, int desired) {
+		int guess = feedforward(inputs);
+		float error = desired - guess;
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] += c * error * inputs[i];
+		}
+	}
+}

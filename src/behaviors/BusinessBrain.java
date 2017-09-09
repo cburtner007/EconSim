a12db32da2b73d121ffed3ad9e12eb12e8fb3734
cburtner007 <cburtner007@gmail.com>
2017-09-09 08:54:26 -0400

    Business beginnings

---
+++ src/behaviors/BusinessBrain.java
@@ -0,0 +1,18 @@
+package behaviors;
+
+import Main.Business;
+
+public class BusinessBrain extends Brain{
+	private Business theBusiness;
+	private int operatingLevel = 0;
+	public BusinessBrain(Business b){
+		this.theBusiness = b;
+	}
+	
+	@Override
+	public void behave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
